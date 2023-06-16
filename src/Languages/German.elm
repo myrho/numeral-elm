b94d0395b6da5cd32a1abb3abcfe3f6bd9158b73
Matthias <git@myrho.net>
2023-06-16 12:27:47 +0200

    Mil -> Mio

--- src/Languages/German.elm
+++ src/Languages/German.elm
@@ -1,8 +1,9 @@
-module Languages.German exposing(lang)
+module Languages.German exposing (lang)
 
 {-| German language configuration.
 
 @docs lang
+
 -}
 
 import Language exposing (..)
@@ -10,42 +11,43 @@ import Language exposing (..)
 
 germanOrdinal : Ordinal
 germanOrdinal number =
-  "."
+    "."
 
 
 {-| Configuration data.
 
     lang =
-      { delimiters=
-        { thousands="."
-        , decimal=","
+        { delimiters =
+            { thousands = "."
+            , decimal = ","
+            }
+        , abbreviations =
+            { thousand = "Tsd."
+            , million = "Mio."
+            , billion = "Mrd."
+            , trillion = "Bil."
+            }
+        , ordinal = germanOrdinal
+        , currency =
+            { symbol = "€"
+            }
         }
-      , abbreviations=
-        { thousand="Tsd."
-        , million="Mil."
-        , billion="Mrd."
-        , trillion="Bil."
-        }
-      , ordinal=germanOrdinal
-      , currency=
-        { symbol="€"
-        }
-      }
+
 -}
 lang : Language
 lang =
-  { delimiters=
-    { thousands="."
-    , decimal=","
+    { delimiters =
+        { thousands = "."
+        , decimal = ","
+        }
+    , abbreviations =
+        { thousand = "Tsd."
+        , million = "Mio."
+        , billion = "Mrd."
+        , trillion = "Bil."
+        }
+    , ordinal = germanOrdinal
+    , currency =
+        { symbol = "€"
+        }
     }
-  , abbreviations=
-    { thousand="Tsd."
-    , million="Mil."
-    , billion="Mrd."
-    , trillion="Bil."
-    }
-  , ordinal=germanOrdinal
-  , currency=
-    { symbol="€"
-    }
-  }
